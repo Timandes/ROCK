eebda68e0d18b833961cf52ac58d90e0199d49a1
yaoweixu0916-ops <yaowei.xu0916@gmail.com>
2026-07-13 10:15:20 +0800

    upgrade image version v0.17.0 (#9)
    * mcp_env dump support query
    
    * upgrade image version v0.17.0
    
    ---------
    
    Co-authored-by: 耀威 <xuyaowei.xyw@alibaba-inc.com>

--- rock/sdk/mcp/rock_runtime.py
+++ rock/sdk/mcp/rock_runtime.py
@@ -67,7 +67,7 @@ class RockRuntimeConfig:
             api_key=api_key,
             image=os.getenv(
                 "ROCK_SANDBOX_IMAGE",
-                "rock-instances-registry-vpc.cn-shanghai.cr.aliyuncs.com/instance/rock-mcp-base:v0.16.0",
+                "rock-instances-registry-vpc.cn-shanghai.cr.aliyuncs.com/instance/rock-mcp-base:v0.17.0",
             ),
             user_id=user_id,
             experiment_id=os.getenv("ROCK_EXPERIMENT_ID", "mcpenv"),
